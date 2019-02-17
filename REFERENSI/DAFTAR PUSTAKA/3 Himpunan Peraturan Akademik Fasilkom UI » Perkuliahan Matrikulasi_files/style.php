ea98f461a887176d974d2f185dcdd5a85887ac95
YodiYanwar <yodi.yanwar@gmail.com>
2019-02-17 08:08:12 +0700

    update

---
+++ REFERENSI/DAFTAR PUSTAKA/3 Himpunan Peraturan Akademik Fasilkom UI » Perkuliahan Matrikulasi_files/style.php
@@ -0,0 +1,29 @@
+/*
+  CSS generated via the Suckerfish WordPress Plugin ... http://ryanhellyer.net/2008/01/14/suckerfish-wordpress-plugin/
+
+  If you would like a similar dropdown for your own site, then please contact Ryan Hellyer for
+  information about getting your own custom designed dropdown menu ... http://ryanhellyer.net/contact/
+*/
+
+
+#suckerfishnav {background:#bbb;font-size:13px;font-family:verdana,sans-serif;font-weight:bold}
+#suckerfishnav, #suckerfishnav ul {float:left;list-style:none;line-height:20px;padding:0;border:1px solid #bbb;margin:0 0 1px 0;width:100%}
+#suckerfishnav a {display:block;color:#fff;text-decoration:none;padding:0px 25px}
+#suckerfishnav li {float:left;padding:0}
+#suckerfishnav li ul {position:absolute;left:-999em;height:auto;width:130px;font-weight:normal;margin:0;line-height:1;-moz-opacity:1;opacity:1;khtml-opacity:1}
+#suckerfishnav li li {padding-right:1px;width:130px;border-bottom:1px solid #aaa}
+#suckerfishnav li li a {padding:3px 10px}
+#suckerfishnav li ul li {font-weight:normal;font-family:verdana,sans-serif}
+#suckerfishnav li ul li a {font-size:13px; color:#fff}
+#suckerfishnav li:hover ul li a {color:#fff}
+#suckerfishnav li ul ul {margin:-20px 0 0 131px}
+#suckerfishnav li ul li:hover {background:#99CCFF}
+#suckerfishnav li ul li:hover a {color:#fff}
+#suckerfishnav li ul li:hover li a {color:#fff}
+#suckerfishnav li ul li li:hover a {color:#fff}
+#suckerfishnav li ul li ul li:hover ul li a {color:#fff}
+#suckerfishnav li ul li ul li:hover ul li:hover a {color:#fff}
+#suckerfishnav li:hover ul ul, #suckerfishnav li:hover ul ul ul, #suckerfishnav li.sfhover ul ul, #suckerfishnav li.sfhover ul ul ul {left:-999em}
+#suckerfishnav li:hover ul, #suckerfishnav li li:hover ul, #suckerfishnav li li li:hover ul, #suckerfishnav li.sfhover ul, #suckerfishnav li li.sfhover ul, #suckerfishnav li li li.sfhover ul {left:auto;background:#bbb}
+#suckerfishnav li:hover, #suckerfishnav li.sfhover {background:#99CCFF}
+#suckerfishnav li:hover a, #suckerfishnav li.sfhover a {color:#fff}
